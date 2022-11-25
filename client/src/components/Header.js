3bc70444e056af7bb7c709929d20164b621103a4
Nikhil Thapa <tnikhil.official@gmail.com>
2022-11-25 21:30:24 +0545

    work distribution for header and footer components

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,8 +3,50 @@ import '../assets/styles/Header.css'
 
 export default function Header() {
     return (
-        <div className='header'>
-            <h1>Header component</h1>
-        </div>
+        <header>
+            <figure className='logo__container'>
+                <img src={headIco} alt='' />
+            </figure>
+            <nav className='nav__options'>
+                <ul>
+                    <div className='nav__option'>
+                        <button className='btn__design'>DESIGN</button>
+                        <div className='nav__dropdown'>
+                            <li>House Plans</li>
+                        </div>
+                    </div>
+
+                    <div className='nav__option'>
+                        <button className='btn__design'>BUILD</button>
+                        <div className='nav__dropdown'>
+                            <li>Resedential Build</li>
+                            <li>Commercial Build</li>
+                        </div>
+                    </div>
+                    <div className='nav__option'>
+                        <button className='btn__design'>DEVELOPMENT</button>
+                        <div className='nav__dropdown'>
+                            <li>Resedential DEVELOPMENT</li>
+                            <li>Commercial DEVELOPMENT</li>
+                        </div>
+                    </div>
+                    <div className='nav__option'>
+                        <button className='btn__design'>ACTIVE LISTINGS</button>
+                        <div className='nav__dropdown'>
+                            <li>Rental Space for Lease</li>
+                        </div>
+                    </div>
+                    <div className='nav__option'>
+                        <li>OUR TEAM</li>
+                    </div>
+                    <div className='nav__option'>
+                        <li>PORTFOLIO</li>
+                    </div>
+                    <div className='nav__option'>
+                        <li>CONTACT US</li>
+                    </div>
+                </ul>
+            </nav>
+        </header>
     )
 }
