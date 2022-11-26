661e887ddf6d681219732b3d050b27188c8072f8
Roshan Sadha Sanker <70261891+RoshanBro77@users.noreply.github.com>
2022-11-26 18:28:57 +0545

    created structure of header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,49 +5,67 @@ import headIco from '../assets/images/skidmore.png'
 export default function Header() {
     return (
         <header>
-            <figure className='logo__container'>
-                <img src={headIco} />
-            </figure>
-            <nav className='nav__options'>
-                <ul>
-                    <div className='nav__option'>
-                        <button className='btn__design'>DESIGN</button>
-                        <div className='nav__dropdown'>
-                            <li>House Plans</li>
+            <div className='header__contains marginLeftRight'>
+                <figure className='logo__container'>
+                    <img src={headIco} height='60px' />
+                </figure>
+                <nav className='nav__options'>
+                    <ul>
+                        <div className='nav__option'>
+                            <button className='btn__design'>DESIGN</button>
+                            <div className='nav__dropdown'>
+                                <ul>
+                                    <li className='nav__drowdown__content'>
+                                        House Plans
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className='nav__option'>
-                        <button className='btn__design'>BUILD</button>
-                        <div className='nav__dropdown'>
-                            <li>Resedential Build</li>
-                            <li>Commercial Build</li>
+                        <div className='nav__option'>
+                            <button className='btn__design'>BUILD</button>
+                            <div className='nav__dropdown'>
+                                <ul>
+                                    <li className='nav__drowdown__content'>
+                                        Resedential Build
+                                    </li>
+                                    <li className='nav__drowdown__content'>
+                                        Commercial Build
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                    <div className='nav__option'>
-                        <button className='btn__design'>DEVELOPMENT</button>
-                        <div className='nav__dropdown'>
-                            <li>Resedential DEVELOPMENT</li>
-                            <li>Commercial DEVELOPMENT</li>
+                        <div className='nav__option'>
+                            <button className='btn__design'>DEVELOPMENT</button>
+                            <div className='nav__dropdown'>
+                                <ul>
+                                    <li className='nav__drowdown__content'>
+                                        Resedential Development
+                                    </li>
+                                    <li className='nav__drowdown__content'>
+                                        Commercial Development
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                    <div className='nav__option'>
-                        <button className='btn__design'>ACTIVE LISTINGS</button>
-                        <div className='nav__dropdown'>
-                            <li>Rental Space for Lease</li>
+                        <div className='nav__option'>
+                            <button className='btn__design'>
+                                ACTIVE LISTINGS
+                            </button>
+                            <div className='nav__dropdown'>
+                                <ul>
+                                    <li className='nav__drowdown__content'>
+                                        Rental Space for Lease
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                    <div className='nav__option'>
-                        <li>OUR TEAM</li>
-                    </div>
-                    <div className='nav__option'>
-                        <li>PORTFOLIO</li>
-                    </div>
-                    <div className='nav__option'>
-                        <li>CONTACT US</li>
-                    </div>
-                </ul>
-            </nav>
+                        <div className='btn__design'>OUR TEAM</div>
+                        <div className='btn__design'>PORTFOLIO</div>
+                        <div className='btn__design'>CONTACT US</div>
+                    </ul>
+                </nav>
+            </div>
         </header>
     )
 }
